ec8739bfd4994ab7fc53af09b089bc97f7b9a6bb
Timothy Li <himynameistimli@gmail.com>
2020-12-11 08:36:16 +0800

    Package cleanup, renamed announcement modules

--- src/Announcement/Announcement.jsx
+++ src/Announcement/Announcement.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import convert from 'htmr';
-import { useRouter } from 'next/router';
 import Cookies from 'universal-cookie';
-import Styles from './Announcement.scss';
+import Styles from './Announcement.module.scss';
 import { messages } from './data';
 
 function Announcement({ className, ...props }) {
   const cookieName = 'cc-read-announcements';
-  const router = useRouter();
   const [message, setMessage] = useState(null);
   const cookies = new Cookies();
 
@@ -21,23 +19,12 @@ function Announcement({ className, ...props }) {
     }
   });
 
-  // Custom override
-  /*    if (router.query.custom_message) {
-      setMessage({
-        message: router.query.custom_message
-      });
-    }
-    // useEffect with followed by an empty array will only load on mount/unmount
-    // useEffect folloed by a var will only load when that var changes
-  }, [router]); */
-
   const closeAnnouncement = (name) => {
     if (name) {
       const read = cookies.get(cookieName) || [];
       read.push(name);
       cookies.set(cookieName, read);
     }
-    router.push(router.asPath);
     setMessage(null);
   };
 
@@ -45,9 +32,7 @@ function Announcement({ className, ...props }) {
     (message && (
       <div className={classNames(Styles.container, className)}>
         <div className={Styles.text}>
-          <p className={Styles.content}>
-            {convert(message.message || router.query.custom_message)}
-          </p>
+          <p className={Styles.content}>{convert(message.message)}</p>
         </div>
         <div className={Styles.close}>
           <span
