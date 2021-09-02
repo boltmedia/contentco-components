b6a60db22de116ac09d860eac087acbf1f4273e2
Timothy Li <himynameistimli@gmail.com>
2021-09-02 16:20:21 +0800

    Add additionalInputContainer logic

--- src/Form/InputContainer.jsx
+++ src/Form/InputContainer.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import classNames from 'classnames';
 // import PropTypes from 'prop-types';
 import Styles from './Styles.module.scss';
-const InputContainer = ({ className, children, ...props }) => {
+const InputContainer = ({ className, children, noMargin, ...props }) => {
   return (
-    <div className={classNames(Styles.inputContainer, className)} {...props}>
+    <div
+      className={classNames(
+        Styles.inputContainer,
+        noMargin && Styles.noMargin,
+        className
+      )}
+      {...props}
+    >
       {children}
     </div>
   );
